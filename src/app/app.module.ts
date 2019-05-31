9f5791ee18eccbf8cfe1aa0005c6313b25920a12
Yuvraj2300 <yuvraj_2300@hotmail.com>
2019-05-31 13:33:15 +0530

    5 files modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatListModule } from '@angular/material/list';
 
 import 'hammerjs';
 
@@ -11,7 +12,7 @@ import { HelloComponent } from './hello.component';
 import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, BrowserAnimationsModule, MatToolbarModule ],
+  imports:      [ BrowserModule, FormsModule, BrowserAnimationsModule, MatToolbarModule, MatListModule ],
   declarations: [ AppComponent, HelloComponent, MenuComponent ],
   bootstrap:    [ AppComponent ]
 })
